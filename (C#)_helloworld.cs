cf9072447a5f8578a90ee7a719f194a2791fcb86
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-03-06 23:17:34 +0530

    Update code format

--- (C#)_helloworld.cs
+++ (C#)_helloworld.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace HelloWorld
-{
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
+namespace HelloWorld{
+	public class Program{
+		public static void Main(string[] args){
 			Console.WriteLine("Hello World!");
 		}
 	}
